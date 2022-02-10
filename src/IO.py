617ddc2ddef0e91381a204180104d582f8bfe11e
DumbInACan <marco@santana.nyc>
2022-02-10 10:33:22 -0500

    IO class for saving and loading the language models

---
+++ src/IO.py
@@ -0,0 +1,17 @@
+#!/bin/python
+import pickle
+
+UNIGRAM = 0
+BIGRAM = 1
+ADDONE = 2
+
+
+class IO:
+    """"This class will handle saving and loading the vocabs for the language models"""
+    _models = "unigram.vocab", "bigram.vocab", "bigram_addone.vocab"
+
+    def save(self, model, vocab):
+        return pickle.dump(vocab, open("../var/models/" + self._models[model], "wb"))
+
+    def load(self, model):
+        return pickle.load(open("../var/models/" + self._models[model], "rb"))
